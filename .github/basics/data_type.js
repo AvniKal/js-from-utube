5507a653e8c54c6548d9b18c6eac93d981a8dc92
AvniKal <kalawatiaavni@gmail.com>
2025-06-10 05:51:20 +0000

    data

---
+++ .github/basics/data_type.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3+3)   we are using node js, not browser
+console.log(3+3)
+console.log("avni")
+
+let name="avni"
+let age=18
+let isLoggedIn= true
+
+// number = 2 to the power 53
+// bigint
+// string ( double quotes)
+// boolean ( true/false)
+// null ( standalone value). representation of empty value
+// undefine (value is not yet assigned)
+// symbol (unique)
+
+// objects
+
+console.log( typeof "hitesh");  //string
+console.log(typeof age)    // number
+console.log(typeof null)  // object
+console.log(typeof undefined) // undefined
+
+
+

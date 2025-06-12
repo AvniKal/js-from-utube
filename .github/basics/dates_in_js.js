5279bfe72fac573858707cd6c9acf4d2bb9f018a
AvniKal <kalawatiaavni@gmail.com>
2025-06-12 14:02:34 +0000

    dates

--- .github/basics/dates_in_js.js
+++ .github/basics/dates_in_js.js
@@ -6,5 +6,27 @@ console.log(myDate.toDateString())
 console.log(myDate.toLocaleString())
 console.log(typeof myDate)
 
-let myCreatedDate= new Date(2023,0,23)
-console.log(myCreatedDate.toDateString())
+//let myCreatedDate= new Date(2023,0,23)
+// let myCreatedDate= new Date(2023,0,23,5,3)
+// let myCreatedDate= new Date("2023-01-14")
+let myCreatedDate= new Date("01-14-2023")
+
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp= Date.now()       // exact time batate when we want to make quizes, who came first
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime())       // convert to milliseconds for comparision
+console.log(Math.floor(Date.now()/1000))     // convert to seconds
+
+let newDate=new Date();
+console.log(newDate)
+console.log(newDate.getMonth()+1)       // jan is 0
+console.log(newDate.getDay())
+
+// string interpolation matlab back ticks ` ` 
+// ` ${newDate.getMonth()} and the day is ${newDate.getDay()}`
+
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+})

252ef5310a66e164d9b1b38978dce3aa8364a54d
ohhh_nooo <kalawatiaavni@gmail.com>
2025-06-22 19:02:08 +0000

    four

--- .github/04_flow-of-control/four.js
+++ .github/04_flow-of-control/four.js
@@ -6,3 +6,31 @@ const values= coding.forEach((item) => {      // call back function ka naam nhi 
 })
 
 console.log(values)   // forEach koi value return nhi karta h output is undefined   
+
+
+
+
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+
+const newNums1= myNums.filter((num) => num>4)
+console.log(newNums1)    // filter terturn karta h unlike forEach
+
+const newNums= myNums.filter((num) => {
+    return num>4
+})
+
+const arr=[]
+newNums.forEach((num) =>{
+    if(num>4)
+        arr.push(num)
+})
+console.log(arr)
+
+
+
+
+
+
+// const userBooks = books.filter( (bk) => bk.genre==="history"). // bk is array of objects
+
